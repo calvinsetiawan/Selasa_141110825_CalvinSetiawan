63a137b0cb479200ab80fcbac32eeaa540faffe7
calvinsetiawan <fairytaicalvin@gmail.com>
2016-10-26 23:04:22 +0700

    versi 1.0.0

---
+++ Latihan_1_2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Latihan_1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.LargeChange = 1;
+            this.vScrollBar1.Location = new System.Drawing.Point(64, 50);
+            this.vScrollBar1.Maximum = 10;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(19, 163);
+            this.vScrollBar1.TabIndex = 0;
+            this.vScrollBar1.Value = 10;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_scroll);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(180, 95);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.LargeChange = 1;
+            this.vScrollBar2.Location = new System.Drawing.Point(472, 50);
+            this.vScrollBar2.Maximum = 10;
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(19, 163);
+            this.vScrollBar2.TabIndex = 0;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar2_scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 355);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.vScrollBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Latihan_1_2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_1_2
+{
+    public partial class Form1 : Form
+    {
+        int min = 0, max = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void vScrollBar(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_scroll(object sender, ScrollEventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            label1.Text = vScrollBar1.Value.ToString();
+            if (vScrollBar2.Value > vScrollBar1.Value)
+            {
+                vScrollBar1.Value = vScrollBar2.Value;
+                
+            }
+            dateTimePicker1.MaxDate = now.AddYears(vScrollBar1.Value - vScrollBar1.Maximum).Date;
+            dateTimePicker1.MinDate = now.AddYears(vScrollBar2.Value - vScrollBar2.Maximum).Date;
+        }
+
+        private void vScrollBar2_scroll(object sender, ScrollEventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            label2.Text = vScrollBar2.Value.ToString();
+            if (vScrollBar2.Value > vScrollBar1.Value)
+            {
+                vScrollBar1.Value = vScrollBar2.Value;
+            }
+            dateTimePicker1.MaxDate = now.AddYears(vScrollBar1.Value - vScrollBar1.Maximum).Date;
+            dateTimePicker1.MinDate = now.AddYears(vScrollBar2.Value - vScrollBar2.Maximum).Date;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        
+    }
+}
